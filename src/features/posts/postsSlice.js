aecf622c5ef1428c28a983c41c999bcbf31d17b0
Shakil57375 <shakil57375@gmail.com>
2024-11-29 12:22:20 +0600

    stored configered

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,7 @@
+import { createEntityAdapter } from "@reduxjs/toolkit"
+
+const postAdapter = createEntityAdapter({
+    sortComparer : (a, b) => b.date.localeCompare(a.date)
+})
+
+const initialState = postAdapter.getInitialState()
