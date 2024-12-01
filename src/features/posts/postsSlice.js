024d80e9747909e4d00f64bbf47bec65805e3f19
Shakil57375 <shakil57375@gmail.com>
2024-12-01 19:23:53 +0600

    useGetPostsByUserIdQuery added

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -2,11 +2,11 @@ import { createEntityAdapter, createSelector } from "@reduxjs/toolkit";
 import { apiSlice } from "../api/apiSlice";
 import { sub } from "date-fns";
 
-const postAdapter = createEntityAdapter({
-  sortComparer: (a, b) => b.date.localeCompare(a.date),
-});
+const postsAdapter = createEntityAdapter({
+    sortComparer: (a, b) => b.date.localeCompare(a.date)
+})
 
-const initialState = postAdapter.getInitialState();
+const initialState = postsAdapter.getInitialState();
 
 export const extendedApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
@@ -28,17 +28,38 @@ export const extendedApiSlice = apiSlice.injectEndpoints({
           return post;
         });
         // Correct the argument order
-        return postAdapter.setAll(initialState, loadedPosts);
+        return postsAdapter.setAll(initialState, loadedPosts);
       },
       providesTags: (result, error, arg) => [
         { type: "Post", id: "LIST" },
         ...result.ids.map((id) => ({ type: "Post", id })),
       ],
     }),
+    getPostsByUserId: builder.query({
+        query: id => `/posts/?userId=${id}`,
+        transformResponse: responseData => {
+            let min = 1;
+            const loadedPosts = responseData.map(post => {
+                if (!post?.date) post.date = sub(new Date(), { minutes: min++ }).toISOString();
+                if (!post?.reactions) post.reactions = {
+                    thumbsUp: 0,
+                    wow: 0,
+                    heart: 0,
+                    rocket: 0,
+                    coffee: 0
+                }
+                return post;
+            });
+            return postsAdapter.setAll(initialState, loadedPosts)
+        },
+        providesTags: (result, error, arg) => [
+            ...result.ids.map(id => ({ type: 'Post', id }))
+        ]
+    }),
   }),
 });
 
-export const { useGetPostsQuery } = extendedApiSlice;
+export const { useGetPostsQuery, useGetPostsByUserIdQuery } = extendedApiSlice;
 
 // returns the query result object
 export const selectPostsResult = extendedApiSlice.endpoints.getPosts.select();
@@ -54,4 +75,4 @@ export const {
   selectById: selectPostById,
   selectIds: selectPostIds,
   // Pass in a selector that returns the posts slice of state
-} = postAdapter.getSelectors((state) => selectPostsData(state) ?? initialState);
+} = postsAdapter.getSelectors((state) => selectPostsData(state) ?? initialState);
