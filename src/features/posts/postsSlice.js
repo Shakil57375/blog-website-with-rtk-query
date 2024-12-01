97161e487e94ecfb0477ab18d37f60b4534c5ba6
Shakil57375 <shakil57375@gmail.com>
2024-12-01 17:11:03 +0600

    updated

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,35 +1,44 @@
-import { createEntityAdapter } from "@reduxjs/toolkit"
-import { apiSlice } from "../api/apiSlice"
-import {sub} from "date-fns"
+t import { createEntityAdapter } from "@reduxjs/toolkit";
+import { apiSlice } from "../api/apiSlice";
+import { sub } from "date-fns";
 const postAdapter = createEntityAdapter({
-    sortComparer : (a, b) => b.date.localeCompare(a.date)
-})
+  sortComparer: (a, b) => b.date.localeCompare(a.date),
+});
 
-const initialState = postAdapter.getInitialState( )
+const initialState = postAdapter.getInitialState();
 
 export const extendedApiSlice = apiSlice.injectEndpoints({
-    endpoints : builder =>({
-        getPosts : builder.query({
-            query : () => "/posts",
-            transformResponse : responseData =>{
-                let min = 1;
-                const loadedPosts = responseData.map(post =>{
-                    if(!post?.date) post.date = sub(new Date(), {minutes : min++}).toString();
-                    if(!post?.reactions) post.reactions = {
-                        thumbsUp : 0,
-                        wow : 0 ,
-                        heart : 0,
-                        rocket : 0,
-                        coffee : 0
-                    }
-                    return post;
-                });
-                return postAdapter.setAll(loadedPosts, initialState);
-            },
-            providesTags : (result, error, arg) =>[
-                {type : "Post", id : "LIST"},
-                ...result.ids.map(id => ({type : "Post", id}))
-            ]
-        })
-    })
-})
+  endpoints: (builder) => ({
+    getPosts: builder.query({
+      query: () => "/posts",
+      transformResponse: (responseData) => {
+        let min = 1;
+        const loadedPosts = responseData.map((post) => {
+          if (!post?.date)
+            post.date = sub(new Date(), { minutes: min++ }).toString();
+          if (!post?.reactions)
+            post.reactions = {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
+            };
+          return post;
+        });
+        return postAdapter.setAll(loadedPosts, initialState);
+      },
+      providesTags: (result, error, arg) => [
+        { type: "Post", id: "LIST" },
+        ...result.ids.map((id) => ({ type: "Post", id })),
+      ],
+    }),
+  }),
+});
+
+export const { useGetPostsQuery } = extendedApiSlice;
+
+
+// returns the query result object
+export const selectPostsResult = extendedApiSlice.endpoints.getPosts.select()
+
